1d5d8c31555dd1193749eeba2d28e2a26e64cdd6
pythonhaja <31542848+pythonhaja@users.noreply.github.com>
2021-09-13 22:44:18 +0530

    Add files via upload

---
+++ Python fundamental  96 Class 1 (1).py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print ("this is my new coding class am  absolute beginner")
+
+
+# In[1]:
+
+
+#understanding variables in python 
+
+x = '5'
+
+print (x)
+
+
+# # Rule no: 1   No blank spaces
+# 
+# 
+# firstname = " raja"
+#  print (firstname )
+
+# In[25]:
+
+
+#valid code 
+name  = "raja"
+ print(name)
+
+
+# In[28]:
+
+
+R 2 :  Name should not start with a number 
+  #invalid code   
+    1name='naveen'
+    print(name)
+    
+    
+
+
+# In[29]:
+
+
+name = 'naveen'
+print(name)
+
+
+# In[30]:
+
+
+#rule 3 : V.n should not be contain any specific character 
+
+# invalid error code 
+
+name@='john'
+print(name@)
+
+
+# In[15]:
+
+
+# correct code 
+
+name='john'
+print(name)
+
+
+# In[19]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ python day 3 .py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+fullname = 'athira reji'
+print(fullname.title())
+print(fullname.upper())
+print(fullname.lower())
+
+
+# In[15]:
+
+
+firstname = 'sachin'
+    
+lastname = 'jadhav'
+
+fullname = f"{firstname}{lastname}"
+print (fullname)
+print(fullname.title())
+message=f"keep up the good work,{fullname.title()}"
+print(message)
+
+
+# In[1]:
+
+
+#adding whitespace to string 
+    print("favorite_language:python,c,java,pascal")
+
+ 
+    print("favorite_languages:\n\t python,\n\t c\,n\t java,\n\t pascal")
+
+
+# In[23]:
+
+
+#Removing whitespaces from string 
+
+name1 = 'athira     '
+name2 = '    reji'
+name3 = '  roshan  '
+
+print(name1.lstrip())
+print(name2.rstrip())
+print(name3.strip())
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
